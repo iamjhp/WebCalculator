f2f50edd0316a1f7062afd81ded40b10302f15c5
iamjhp <jooho.park@outlook.com>
2022-02-01 05:29:47 +0100

    creadted buttons and display in html file

--- script.js
+++ script.js
@@ -13,3 +13,24 @@ function mult(a, b) {
 function div(a, b) {
     return a / b
 }
+
+function operate (operator, num1, num2) {
+    let res
+    switch (operator) {
+        case "plus":
+            res = add(num1, num2)
+            break
+        case "minus":
+            res = sub(num1, num2)
+            break
+        case "multiply":
+            res = mult(num1, num2)
+            break
+        case "divide":
+            res = div(num1, num2)
+            break
+        default:
+            res = null
+    }
+    return res
+}
